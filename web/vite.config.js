ed852257a11c4d20231baf3bdeabd3d72210ba6d
Diego <diegobcuadros@gmail.com>
2025-07-15 11:27:13 -0400

    Set up viteconfig for multipage app setup

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [tailwindcss(), react()],
+  esbuild: { legalComments: "none" },
+  resolve: { alias: { "@": resolve(__dirname, "./src") } },
+  build: {
+    rollupOptions: {
+      input: {
+        landing: resolve(__dirname, "landing/index.html"),
+        app: resolve(__dirname, "app/index.html"),
+      },
+    },
+  },
+  base: "./",
+});

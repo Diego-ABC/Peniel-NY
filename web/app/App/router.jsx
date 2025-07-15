8d39fb58050c9370563c804ee6d4b7da7a844bbc
Diego <diegobcuadros@gmail.com>
2025-07-15 14:16:43 -0400

    Set up express server for separate vite dev servers for landing and crm app

--- web/app/App/router.jsx
+++ web/app/App/router.jsx
@@ -9,5 +9,6 @@ export default createBrowserRouter(
     <Route>
       <Route path="/" element={<h1>crm app site</h1>} />
     </Route>
-  )
+  ),
+  { basename: import.meta.env.VITE_APP_BASE_PATH }
 );
--- web/vite.config.js
+++ web/vite.config.js
@@ -1,20 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  esbuild: { legalComments: "none" },
-  resolve: { alias: { "@": resolve(__dirname, "./src") } },
-  build: {
-    rollupOptions: {
-      input: {
-        landing: resolve(__dirname, "landing/index.html"),
-        app: resolve(__dirname, "app/index.html"),
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const appBase = env.VITE_APP_BASE_PATH || "/app";
+  const appDir = appBase.replace(/^\//, ""); // "app"
+
+  return defineConfig({
+    plugins: [tailwindcss(), react()],
+    esbuild: { legalComments: "none" },
+    resolve: { alias: { "@": resolve(__dirname, "./app") } },
+    build: {
+      rollupOptions: {
+        input: {
+          landing: resolve(__dirname, "landing/index.html"),
+          [appDir]: resolve(__dirname, `${appDir}/index.html`),
+        },
       },
     },
-  },
-  base: "./",
-});
+    base: "./",
+  });
+};

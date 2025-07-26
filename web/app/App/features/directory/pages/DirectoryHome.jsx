8bbc20dbd1835ace3e17a54b99401d120eb80e16
Diego <diegobcuadros@gmail.com>
2025-07-26 00:12:39 -0400

    Add real directory main components and contacts through hook

--- web/app/App/features/directory/pages/DirectoryHome.jsx
+++ web/app/App/features/directory/pages/DirectoryHome.jsx
@@ -1,3 +1,15 @@
+import DirectoryHeader from "../components/DirectoryHeader";
+import ContactList from "../components/ContactList";
+import { useActionData, useNavigation } from "react-router-dom";
+import { useEffect } from "react";
+import useLiveContactsList from "../hooks/useLiveContactsList";
+
 export default function DirectoryHome() {
-  return <>directory home page</>;
+  const contacts = useLiveContactsList();
+  return (
+    <>
+      <DirectoryHeader />
+      <ContactList contacts={contacts} />
+    </>
+  );
 }
